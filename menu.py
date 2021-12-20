9668b68a1bc90381c1ab4466ecf2b3190e90ae78
no-glue <nikolapav1985@gmail.com>
2021-12-20 15:30:28 +0100

    nk menu py gizmo path

--- menu.py
+++ menu.py
@@ -1,5 +1,8 @@
+import os
+
 if __name__ == "__main__":
     '''
         menu.py called any time for nuke gui
     '''
     print(">>> NUKE MENU PY")
+    print(">>> {} {}".format(os.environ["NUKE_PATH_GIZMOS"],os.environ["NUKE_PATH_NK"]))
