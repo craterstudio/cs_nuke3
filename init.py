f0321b07210c92a17ad25ac2a89245a2b77f1211
no-glue <nikolapav1985@gmail.com>
2021-12-20 15:17:46 +0100

    nk init meny py comment

---
+++ init.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    '''
+        init.py called any time for nuke
+    '''
+    print(">>> NUKE INIT PY")
---
+++ menu.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    '''
+        menu.py called any time for nuke gui
+    '''
+    print(">>> NUKE MENU PY")
