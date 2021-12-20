a41d67226d5f7f4cb5480958033dd9c0639164d3
no-glue <nikolapav1985@gmail.com>
2021-12-20 16:10:49 +0100

    nk gizmo list py basic

---
+++ python/gizmo_add.py
@@ -0,0 +1,9 @@
+class GizmoAdd(object):
+    '''
+        class GizmoAdd add gizmo to environment
+    '''
+    def __init__(self):
+        '''
+            class GizmoAdd initialize instance
+        '''
+        pass
---
+++ python/gizmo_list.py
@@ -0,0 +1,9 @@
+class GizmoList(object):
+    '''
+        class GizmoList list gizmos found on the file system
+    '''
+    def __init__(self):
+        '''
+            class GizmoList initialize instance
+        '''
+        pass
