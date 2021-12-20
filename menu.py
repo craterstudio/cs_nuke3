c620344953dff7e36fbd1319ecec41fb5abaecb6
no-glue <nikolapav1985@gmail.com>
2021-12-20 17:03:07 +0100

    nk gizmo list do

--- menu.py
+++ menu.py
@@ -1,10 +1,18 @@
 import os
+from dbg import Dbg
 from gizmo_list import GizmoList
 from gizmo_add import GizmoAdd
+
+def gizmo_list_do():
+   '''
+        gizmo_list_do do a gizmo listing
+   ''' 
+    Dbg d=Dbg()
+
+    dbg.print("NUKE MENU PY ","{} {}".format(os.environ["NK_PATH_GIZMOS"],os.environ["NK_PATH_NK"]))
 
 if __name__ == "__main__":
     '''
         menu.py called any time for nuke gui
     '''
-    print(">>> NUKE MENU PY")
-    print(">>> {} {}".format(os.environ["NK_PATH_GIZMOS"],os.environ["NK_PATH_NK"]))
+    gizmo_list_do()
