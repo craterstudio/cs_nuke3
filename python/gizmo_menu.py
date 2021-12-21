a0d8c312d051612d1fa49feac9d8b1bdfcb0b774
no-glue <nikolapav1985@gmail.com>
2021-12-21 13:00:21 +0100

    nk gizmo menu basic

---
+++ python/gizmo_menu.py
@@ -0,0 +1,24 @@
+class GizmoMenu(object):
+    '''
+        class GizmoMenu add gizmos to menu
+    '''
+    def __init__(self,strr=""):
+        '''
+            class GizmoMenu initialize instance
+        '''
+        self.str=strr
+    def getStr(self):
+        '''
+            class GizmoMenu get str
+        '''
+        return self.str
+    def setStr(self,strr):
+        '''
+            class GizmoMenu set str
+        '''
+        self.str=strr
+    def doStr(self):
+        '''
+            class GizmoMenu do str
+        '''
+        pass
